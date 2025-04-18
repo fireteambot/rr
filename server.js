6d889096985dc00121376ebccce57f7e2bc72e65
Wmm.malshan <111701987+fireteambot@users.noreply.github.com>
2025-04-18 22:13:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 80;  // Port 80 for Azure App Service
 
 app.use('/', createProxyMiddleware({
-  target: 'https://paid.rtxconfigz.shop',  // 3CX panel URL
+  target: 'http://amazon.rtxconfigz.xyz',  // 3CX panel URL
   changeOrigin: true,  // Ensures correct Host header
   logLevel: 'debug',
   onError: (err, req, res) => {
